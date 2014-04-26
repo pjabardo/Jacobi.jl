8c763b0962a1ba02ccb00981ff9d886983ad6a8d
Paulo Jabardo <pjabardo@yahoo.com.br>
2014-04-26 14:36:30 -0300

    Removed the exports from gamma_aux.jl.

--- src/Jacobi.jl
+++ src/Jacobi.jl
@@ -5,13 +5,11 @@ export jacobi, djacobi, jacobi_zeros
 export zgj, zglj, zgrjm, zgrjp, wgj, wglj, wgrjm, wgrjp
 export dgj, dglj, dgrjm, dgrjp, lgj, lglj, lgrjm, lgrjp
 export igj, iglj, igrjm, igrjp
-export rgammanm, rgammaxy, rgammaxnym
 
 
 
 include("jac_poly.jl")
 include("gauss_quad.jl")
-include("gamma_aux.jl")
 
 
 end # module
