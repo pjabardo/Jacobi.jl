171ab8c53effd08c361c88a71e1db521f9f58ffc
Paulo Jabardo <pjabardo@yahoo.com.br>
2015-04-17 10:41:21 -0300

    Forgot to export the new array! functions.

--- src/Jacobi.jl
+++ src/Jacobi.jl
@@ -8,6 +8,8 @@ export lagrange, interp_mat
 export chebyshev, chebyshev2, dchebyshev, dchebyshev2
 export chebyshev_zeros!, chebyshev_zeros, chebyshev2_zeros!, chebyshev2_zeros
 export legendre, dlegendre, legendre_zeros
+export chebyshev!, chebyshev2!, dchebyshev!, dchebyshev2!
+export legendre!, jacobi!, djacobi!
 
 include("jac_poly.jl")
 include("chebyshev.jl")
