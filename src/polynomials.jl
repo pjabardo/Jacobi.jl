35c7593673da8902d55579657546f620a0b7b70e
Paulo José Saiz Jabardo <pjabardo@gmail.com>
2021-02-24 11:45:30 -0300

    Improved doc of poly_* functions
    Now it has examples and follows the guidelines better.

--- src/polynomials.jl
+++ src/polynomials.jl
@@ -4,7 +4,7 @@ using Polynomials
 """
     poly_chebyshev(n, [::Type{T}=Int, [var=:x]])
 
-Create a Chebyshev order of order n `Polynomial` object.
+Create a Chebyshev polynomial of order n `Polynomial` object.
 
 Instead of computing the polynomial at a specific point, 
 use `Polynomials` package to compute the actual polynomial
@@ -17,7 +17,16 @@ Parameters:
 
 # Examples
 ```julia-repl
+julia> p = poly_chebyshev(4)
+Polynomials.Polynomial(1 - 8*x^2 + 8*x^4)
 
+julia> p2 = poly_chebyshev(4, BigInt)
+Polynomials.Polynomial(1 - 8*x^2 + 8*x^4)
+
+julia> p3 = poly_chebyshev(4, Float64, :y)
+Polynomials.Polynomial(1.0 - 8.0*y^2 + 8.0*y^4)
+
+```
 
 """
 function poly_chebyshev(n, ::Type{T}=Int, var=:x) where {T<:Number}
@@ -43,20 +52,61 @@ function poly_chebyshev(n, ::Type{T}=Int, var=:x) where {T<:Number}
 end
 
 """
-Calculate derivative of Chebyshev polynomials coefficients
+    poly_dchebyshev(n, [::Type{T}=Int, [var=:x]])
+
+Create the derivative of Chebyshev polynomial of order n `Polynomial` object.
 
 Instead of computing the polynomial at a specific point, 
 use `Polynomials` package to compute the actual polynomial
+
+Parameters:
+
+ * `n` Order of polynomials
+ * `T` Type of polynomial coefficients
+ * `var` symbol to be used as variable by the polynomial
+
+# Examples
+```julia-repl
+julia> p = poly_dchebyshev(4)
+Polynomials.Polynomial(-16*x + 32*x^3)
+
+julia> p2 = poly_dchebyshev(4, BigInt)
+Polynomials.Polynomial(-16*x + 32*x^3)
+
+julia> p3 = poly_dchebyshev(4, Float64, :y)
+Polynomials.Polynomial(-16.0*y + 32.0*y^3)
+```
 """
 poly_dchebyshev(n, ::Type{T}=Int, var=:x) where {T<:Number} = derivative(poly_chebyshev(n,T, var))
 
 
 
 """
-Calculate the coefficients of Chebyshev polynomials of the second kind
+    poly_chebyshev2(n, [::Type{T}=Int, [var=:x]])
+
+Create a Chebyshev of the second kind  of order n `Polynomial` object.
 
 Instead of computing the polynomial at a specific point, 
 use `Polynomials` package to compute the actual polynomial
+
+Parameters:
+
+ * `n` Order of polynomials
+ * `T` Type of polynomial coefficients
+ * `var` symbol to be used as variable by the polynomial
+
+# Examples
+```julia-repl
+julia> p = poly_chebyshev2(4)
+Polynomials.Polynomial(1 - 12*x^2 + 16*x^4)
+
+julia> p2 = poly_chebyshev2(4, BigInt)
+Polynomials.Polynomial(1 - 12*x^2 + 16*x^4)
+
+julia> p3 = poly_chebyshev2(4, Float64, :y)
+Polynomials.Polynomial(1.0 - 12.0*y^2 + 16.0*y^4)
+```
+
 """
 function poly_chebyshev2(n, ::Type{T}=Int, var=:x) where {T<:Number}
 
@@ -81,19 +131,61 @@ function poly_chebyshev2(n, ::Type{T}=Int, var=:x) where {T<:Number}
 end
 
 """
-Calculate the coefficients of the derivative of Chebyshev polynomials of the second kind
+    poly_dchebyshev2(n, [::Type{T}=Int, [var=:x]])
+
+Create a derivative of Chebyshev polynomial of the second kind  of order n `Polynomial` object.
 
 Instead of computing the polynomial at a specific point, 
 use `Polynomials` package to compute the actual polynomial
+
+Parameters:
+
+ * `n` Order of polynomials
+ * `T` Type of polynomial coefficients
+ * `var` symbol to be used as variable by the polynomial
+
+# Examples
+```julia-repl
+julia> p = poly_dchebyshev2(4)
+Polynomials.Polynomial(-24*x + 64*x^3)
+
+julia> p2 = poly_dchebyshev2(4, BigInt)
+Polynomials.Polynomial(-24*x + 64*x^3)
+
+julia> p3 = poly_dchebyshev2(4, Float64, :y)
+Polynomials.Polynomial(-24.0*y + 64.0*y^3)
+```
+
 """
 poly_dchebyshev2(n, ::Type{T}=Int, var=:x) where {T<:Number} = derivative(poly_chebyshev2(n,T, var))
 
 
 """
-Calculate the coefficients of Legendre polynomials 
+    poly_legendre(n, [::Type{T}=Int, [var=:x]])
+
+Create a Legendre polynomial of order n `Polynomial` object.
 
 Instead of computing the polynomial at a specific point, 
 use `Polynomials` package to compute the actual polynomial
+
+Parameters:
+
+ * `n` Order of polynomials
+ * `T` Type of polynomial coefficients
+ * `var` symbol to be used as variable by the polynomial
+
+# Examples
+```julia-repl
+julia> p = poly_legendre(4)
+Polynomials.Polynomial(0.375 - 3.75*x^2 + 4.375*x^4)
+
+julia> p2 = poly_legendre(4, Rational{Int})
+Polynomials.Polynomial(3//8 - 15//4*x^2 + 35//8*x^4)
+
+julia> p3 = poly_legendre(4, BigFloat, :y)
+Polynomials.Polynomial(0.375 - 3.75*y^2 + 4.375*y^4)
+```
+
 """
 function poly_legendre(n, ::Type{T}=Float64, var=:x) where {T<:Number}
 
@@ -119,10 +211,31 @@ end
 
 
 """
-Calculate the coefficients of the derivativ of Legendre polynomials 
+    poly_dlegendre(n, [::Type{T}=Int, [var=:x]])
+
+Create a derivative of Legendre polynomial of order n `Polynomial` object.
 
 Instead of computing the polynomial at a specific point, 
 use `Polynomials` package to compute the actual polynomial
+
+Parameters:
+
+ * `n` Order of polynomials
+ * `T` Type of polynomial coefficients
+ * `var` symbol to be used as variable by the polynomial
+
+# Examples
+```julia-repl
+julia> p = poly_dlegendre(4)
+Polynomials.Polynomial(-7.5*x + 17.5*x^3)
+
+julia> p2 = poly_dlegendre(4, Rational{Int})
+Polynomials.Polynomial(-15//2*x + 35//2*x^3)
+
+julia> p3 = poly_dlegendre(4, BigFloat, :y)
+Polynomials.Polynomial(-7.5*y + 17.5*y^3)
+```
+
 """
 poly_dlegendre(n, ::Type{T}=Int, var=:x) where {T<:Number} = derivative(poly_legendre(n,T, var))
 
@@ -131,10 +244,32 @@ poly_dlegendre(n, ::Type{T}=Int, var=:x) where {T<:Number} = derivative(poly_leg
 
 
 """
-Calculate the coefficients of Jacobi polynomials 
+    poly_jacobi(n, [::Type{T}=Int, [var=:x]])
+
+Create a Jacobi polynomial of order n `Polynomial` object.
 
 Instead of computing the polynomial at a specific point, 
 use `Polynomials` package to compute the actual polynomial
+
+Parameters:
+
+ * `n` Order of polynomials
+ * `a` Weight α of the Jacobi polynomial
+ * `b` Weight β of the Jacobi polynomial
+ * `T` Type of polynomial coefficients
+ * `var` symbol to be used as variable by the polynomial
+
+# Examples
+```julia-repl
+julia> p = poly_jacobi(4, 1, 1)
+Polynomials.Polynomial(0.625 - 8.75*x^2 + 13.125*x^4)
+
+julia> p2 = poly_jacobi(4, 1, 1, Rational{Int})
+Polynomials.Polynomial(5//8 - 35//4*x^2 + 105//8*x^4)
+
+julia> p3 = poly_jacobi(4, 1, 1, BigFloat, :y)
+Polynomials.Polynomial(0.625 - 8.75*y^2 + 13.125*y^4)
+```
 """
 function poly_jacobi(n, a, b, ::Type{T}=Float64, var=:x) where {T<:Number}
 
@@ -165,9 +300,31 @@ end
 
 
 """
-Calculate the coefficients of the derivative of Jacobi polynomials 
+    poly_djacobi(n, [::Type{T}=Int, [var=:x]])
+
+Create a derivative of Jacobi polynomial of order n `Polynomial` object.
 
 Instead of computing the polynomial at a specific point, 
 use `Polynomials` package to compute the actual polynomial
+
+Parameters:
+
+ * `n` Order of polynomials
+ * `a` Weight α of the Jacobi polynomial
+ * `b` Weight β of the Jacobi polynomial
+ * `T` Type of polynomial coefficients
+ * `var` symbol to be used as variable by the polynomial
+
+# Examples
+```julia-repl
+julia> p = poly_djacobi(4, 1, 1)
+Polynomials.Polynomial(-17.5*x + 52.5*x^3)
+
+julia> p2 = poly_djacobi(4, 1, 1, Rational{Int})
+Polynomials.Polynomial(-35//2*x + 105//2*x^3)
+
+julia> p3 = poly_djacobi(4, 1, 1, BigFloat, :y)
+Polynomials.Polynomial(-17.5*y + 52.5*y^3)
+```
 """
 poly_djacobi(n, a, b, ::Type{T}=Float64, var=:x) where {T<:Number} = derivative(poly_jacobi(n,a,b,T,var))
