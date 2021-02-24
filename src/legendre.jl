2ea3e2df80be492fe1d5035776aeeb049919536c
Paulo José Saiz Jabardo <pjabardo@gmail.com>
2021-02-24 12:17:16 -0300

    Setting up CI and improving docs
    Just copying what I did with the package CurveFit. And improved the docs.

--- src/legendre.jl
+++ src/legendre.jl
@@ -1,10 +1,27 @@
 """ 
-Compute Legendre polynomials
+    legendre(x, b)
+
+Compute Legendre polynomials.
 
 Legendre polynomials are Jacobi polynomials with parameters a and b equals to zero.
 
 Same interface as `jacobi`. `dlegendre` computes the derivative of the Legendre 
 polynomial.
+
+# Example
+```julia-repl
+julia> x = legendre(0.4, 5)
+0.27063999999999994
+
+julia> x = legendre(2//5, 5)
+3383//12500
+
+julia> x = dlegendre(0.4, 5)
+-1.3170000000000002
+
+julia> x = dlegendre(2//5, 5)
+-1317//1000
+```
 """
 function legendre(x, n)
 
